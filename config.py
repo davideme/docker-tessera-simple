f99750c681082c5661cec0bb43eeea024e101e9f
Davide Mendolia <idaf1er@gmail.com>
2015-03-03 15:56:49 +0100

    Added sqlite server to config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
-GRAPHITE_URL  = os.getenv('GRAPHITE_URL', 'http://localhost:8080')
-SECRET_KEY    = os.getenv('SECRET_KEY', '9f981913-4e97-d7c0-d735-eb7fe1ef1463')
-SERVER_PORT   = 80
+GRAPHITE_URL = os.getenv('GRAPHITE_URL', 'http://localhost:8080')
+SECRET_KEY = os.getenv('SECRET_KEY', 'cb55d1a0-fa54-44f9-833a-00dabbe7c5a6')
+SERVER_PORT = 80
 DEFAULT_THEME = 'snow'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////var/lib/graphite/tessera.db'
