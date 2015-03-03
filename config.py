b19e875cb5bdb187c3b622e124f88e172c4966cf
Davide Mendolia <idaf1er@gmail.com>
2015-03-03 16:19:10 +0100

    Change sqlite folder

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ GRAPHITE_URL = os.getenv('GRAPHITE_URL', 'http://localhost:8080')
 SECRET_KEY = os.getenv('SECRET_KEY', 'cb55d1a0-fa54-44f9-833a-00dabbe7c5a6')
 SERVER_PORT = 80
 DEFAULT_THEME = 'snow'
-SQLALCHEMY_DATABASE_URI = 'sqlite:////var/lib/graphite/tessera.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////var/lib/tessera/tessera.db'
